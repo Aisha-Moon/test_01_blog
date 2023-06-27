1cdad11245c5c48f21083526b774f68fd9f208a9
Moon <imtiajhanimoon@gmail.com>
2023-06-27 23:54:22 +0600

    show session flash data in many ways

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -2,8 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\CategoryFormRequest;
+use App\Http\Requests\StoreCategoryRequest;
 use App\Models\Category;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Session;
 
 class CategoryController extends Controller
 {
@@ -35,21 +38,35 @@ class CategoryController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(CategoryFormRequest $request)
     {
+        $request->validated();
     //    dd($request->all());
-    $request->validate([
-    'name' => 'required|max:255',
-    'description' => 'required',
-        ]);
+    // $request->validate([
+    // 'name' => 'required|max:255',
+    // 'description' => 'required',
+    //     ],[
+    //         'name.required'=>"Name must be filled up",
+    //         'description.required'=>"Description must be filled up",
+    //     ]);
         $data['name'] = $request->name;
         $data['description'] = $request->description;
         Category::create([
             'name'=>$request->name,
             'description'=>$request->description,
         ]);
+        //option 1
+        // $request->session()->flash('success', 'Category created successfull.');
+        //option 2
+        // session()->flash('success', 'Category created successfull.');
+        //option 3
+        // Session::flash('success', 'Category created successfull.');
+        // return redirect()->route('categories.index');
 
-        return redirect()->route('categories.index');
+        
+
+        return redirect()->route('categories.index')->with('success','Category created successfull');
+
     }
 
     /**
@@ -90,26 +107,26 @@ class CategoryController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, Category $category)
+    public function update(CategoryFormRequest $request, Category $category)
     {
 
-
+        $request->validated();
         //option 1
        //    dd($request->all());
-        // $data['name'] = $request->name;
-        // $data['description'] = $request->description;
-        // $category -> update($data);
-        // return redirect()->route('categories.index');
+        $data['name'] = $request->name;
+        $data['description'] = $request->description;
+        $category -> update($data);
+        return redirect()->route('categories.index');
  //option 2
         // Category::where ('id',$category->id)->update([
         //     'name'=>$request->name,
         //     'description'=>$request->description,
         // ]);
 
-        return redirect()->route('categories.index');
+        // return redirect()->route('categories.index');
      //option 3
-        $category->update($request->all());  
-        return redirect()->route('categories.index');
+        // $category->update($request->all());  
+        // return redirect()->route('categories.index');
 
     }
 
--- resources/views/backend/category/index.blade.php
+++ resources/views/backend/category/index.blade.php
@@ -7,7 +7,24 @@
 
     <div class="col-md-6">
       <a class="btn btn-info btn-sm" href="{{route('categories.create')}}">Create Categories</a>
-
+      {{-- option1  --}}
+{{-- @if (session('success'))
+    <div class="alert alert-success">
+        {{ session('success') }}
+    </div>
+@endif --}}
+      {{-- option 2 --}}
+{{-- @if (session()->has('success'))
+    <div class="alert alert-success">
+        {{ session()->get('success') }}
+    </div>
+@endif --}}
+{{-- option 3 --}}
+@if (Session::has('success'))
+    <div class="alert alert-success" id="alert">
+        {{ Session::get('success') }}
+    </div>
+@endif
       <h1>Create Category</h1>
       <table class="table table-bordered table-hover table-stripped">
         <thead>
